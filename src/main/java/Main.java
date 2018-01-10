1adc1c5161ed0c21cb03ac0a0043fcea4fee840f
Yinghao Wang <yinghao.a.wang@gmail.com>
2018-01-10 14:11:59 -0800

    webcam and filtering working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,20 @@
-import static org.bytedeco.javacpp.opencv_core.*;
-import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+import org.bytedeco.javacpp.opencv_core.*;
+import org.bytedeco.javacv.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 
+
 public class Main {
-    public static void main(String[] args) {
-        String filename = "asdf.png";
-        IplImage image = cvLoadImage(filename);
-        if (image != null) {
-            cvSmooth(image, image);
-            cvSaveImage(filename, image);
-            cvReleaseImage(image);
+    public static void main(String[] args) throws FrameGrabber.Exception {
+        FrameGrabber grabber = FrameGrabber.createDefault(0);
+        grabber.start();
+
+        Frame grabbedImage;
+        CanvasFrame cFrame = new CanvasFrame("title", CanvasFrame.getDefaultGamma() / grabber.getGamma());
+        while ((grabbedImage = grabber.grab()) != null) {
+            OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
+            Mat mat = converter.convert(grabbedImage);
+            medianBlur(mat, mat, 15);
+            cFrame.showImage(converter.convert(mat));
         }
     }
 }
