fe4682c8e720d69cb04f20e662fae0ff5f7e5241
Yinghao Wang <yinghao.a.wang@gmail.com>
2018-01-09 20:57:28 -0800

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String filename = "asdf.png";
+        IplImage image = cvLoadImage(filename);
+        if (image != null) {
+            cvSmooth(image, image);
+            cvSaveImage(filename, image);
+            cvReleaseImage(image);
+        }
+    }
+}
